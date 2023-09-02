5eaaf7506028ab3faa23b139eb69a1d2e7e653ac
Guille Fernandez <51216187+etrigan16@users.noreply.github.com>
2023-09-02 18:55:51 -0300

    ejercicios nuevos

--- guia1/ej_8.py
+++ guia1/ej_8.py
@@ -1,1 +1,19 @@
+#Ingresar la cantidad de números de la sucesión de Fibonacci a mostrar.
 
+ingresa_numero = int(0)
+primer_numero = int(0)
+segundo_numero = int(1)
+aux_num = int(0)
+
+ingresa_numero = int(input("Ingrese la cantidad de numeros que desea ver de la Sucesion de Fibonacci: "))
+
+print(primer_numero)
+print(segundo_numero)
+for i in range (ingresa_numero):
+    aux_num = primer_numero+segundo_numero
+    primer_numero=segundo_numero
+    segundo_numero=aux_num
+    print(aux_num)
+
+primer_numero = int (0)
+segundo_numero = int (1)
---
+++ guia1/ej_9.py
@@ -0,0 +1,18 @@
+# Multiplicación. Ingresar un número cualquiera por teclado y que muestre su respectiva tabla del 1 al 10.
+
+multiplicando = int(0)
+producto = int(0)
+multiplicador = int(10)
+
+multiplicando = int(input("ingrese el numero que desea multiplicar. El resultado sera la tabla del 10: "))
+
+for i in range (multiplicador):
+    producto = multiplicando*i
+    print(multiplicando, " x ",i)
+    producto = int(0)
+
+
+
+
+
+
